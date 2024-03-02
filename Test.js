42f0e312b721f0f8290e89beffc8347a44eaca6f
atharvamene025 <atharvamene@gmail.com>
2024-03-02 14:30:49 +0530

    updates

---
+++ Test.js
@@ -0,0 +1,45 @@
+
+const inputIntervals=[[1,2,3],[2,3,4,5,6],[8,9,10],[15,16,17,18]]
+
+const mergedIntervals = mergeIntervals(inputIntervals);
+
+function mergeIntervals(inputIntervals){
+
+const total=inputIntervals[0]+ inputIntervals[1]
+
+console.log(total)
+
+
+}
+
+mergeIntervals(inputIntervals)
+
+
+
+
+
+
+
+
+// function mergeIntervals(intervals) {
+    // Your code here
+// }
+
+// const inputIntervals = [
+//     { start: 1, end: 3 },
+//     { start: 2, end: 6 },
+//     { start: 8, end: 10 },
+//     { start: 15, end: 18 }
+// ];
+
+// const mergedIntervals = mergeIntervals(inputIntervals);
+// console.log(mergedIntervals);
+
+// ans :  [
+//     { start: 1, end: 6 },
+//     { start: 8, end: 10 },
+//     { start: 15, end: 18 }
+// ];
+
+
+// Build a todo app, Use localstorage, Add todo, Delete Todo
